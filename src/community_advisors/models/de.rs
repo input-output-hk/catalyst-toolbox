4314980b1c9ff4ed897dfbb604144183f6fbd984
danielsanchezq <daniel.sanchez@iohk.io>
2021-09-29 18:50:14 +0200

    Added base reviews aggregated file models

---
+++ src/community_advisors/models/de.rs
@@ -0,0 +1,7 @@
+use serde::Deserialize;
+
+pub struct ValidAssessments {
+    proposal_id: String,
+    idea_url: String,
+    assessor: String,
+}
---
+++ src/community_advisors/models/mod.rs
@@ -0,0 +1,5 @@
+mod de;
+mod tags;
+
+pub use de::ValidAssessments;
+pub use tags::TagsMap;
