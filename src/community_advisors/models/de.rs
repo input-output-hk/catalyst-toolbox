dd45f12b008560b75efa559dc9a21e6d3fde5a1e
danielsanchezq <daniel.sanchez@iohk.io>
2021-10-01 15:14:43 +0200

    Added deserialization test

--- src/community_advisors/models/de.rs
+++ src/community_advisors/models/de.rs
@@ -5,7 +5,7 @@ use vit_servicing_station_lib::db::models::community_advisors_reviews::ReviewTag
 #[derive(Deserialize)]
 pub struct AdvisorReviewRow {
     pub proposal_id: String,
-    #[serde(alias = "Proposal URL")]
+    #[serde(alias = "Idea URL")]
     pub idea_url: String,
     #[serde(alias = "Assessor")]
     pub assessor: String,
@@ -21,16 +21,22 @@ pub struct AdvisorReviewRow {
     auditability_note: String,
     #[serde(alias = "Auditability Rating")]
     pub auditability_rating: u8,
+    #[serde(alias = "Excellent")]
     pub excellent: u32,
+    #[serde(alias = "Good")]
     pub good: u32,
 }
 
 #[cfg(test)]
 mod tests {
+    use crate::community_advisors::models::AdvisorReviewRow;
+    use crate::utils::csv as csv_utils;
     use std::path::PathBuf;
 
     #[test]
     fn test_deserialize() {
-        let file_path = PathBuf::from("../../../resources/testing/valid_assessments.csv");
+        let file_path = PathBuf::from("./resources/testing/valid_assessments.csv");
+        let data: Vec<AdvisorReviewRow> = csv_utils::load_data_from_csv(&file_path).unwrap();
+        assert_eq!(data.len(), 1);
     }
 }
