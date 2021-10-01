ea316c6f3842802c3af6e8b3279b3b4cd964919c
Daniel Sanchez Quiros <daniel.sanchez@iohk.io>
2021-10-01 10:33:12 +0200

    Added notes and rating

--- src/community_advisors/models/de.rs
+++ src/community_advisors/models/de.rs
@@ -1,7 +1,35 @@
+use super::tags::TagsMap;
 use serde::Deserialize;
+use std::ops::Range;
+use vit_servicing_station_lib::db::models::community_advisors_reviews::ReviewTag;
 
+#[derive(Deserialize)]
 pub struct ValidAssessments {
     proposal_id: String,
+    #[serde(alias = "Proposal URL")]
     idea_url: String,
+    #[serde(alias = "Assessor")]
     assessor: String,
+    #[serde(alias = "Impact / Alignment Note")]
+    impact_alignment_note: String,
+    #[serde(alias = "Impact / Alignment Rating")]
+    impact_alignment_rating: u8,
+    #[serde(alias = "Feasibility Note")]
+    feasibility_note: String,
+    #[serde(alias = "Feasibility Rating")]
+    feasibility_rating: u8,
+    #[serde(alias = "Auditability Note")]
+    auditability_note: String,
+    #[serde(alias = "Auditability Rating")]
+    auditability_rating: u8,
+}
+
+#[cfg(test)]
+mod tests {
+    use std::path::PathBuf;
+
+    #[test]
+    fn test_deserialize() {
+        let file_path = PathBuf::from("./resources/tests/valid_assessments.csv");
+    }
 }
