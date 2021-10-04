e25a3097c1e90c41bb8af9385e0905dc1248ce8a
danielsanchezq <daniel.sanchez@iohk.io>
2021-10-04 11:18:37 +0200

    Removed unused excel and models module Added ProposalRewardsSlots

--- src/rewards/ca/mod.rs
+++ src/rewards/ca/mod.rs
@@ -1,16 +1,7 @@
-mod excel;
 mod funding;
 mod lottery;
-mod models;
 
 use serde::Deserialize;
 
 pub type Ca = String;
 pub type ProposalId = String;
-
-#[derive(Deserialize)]
-pub enum ReviewGrade {
-    Excellent,
-    Good,
-    FilteredOut,
-}
