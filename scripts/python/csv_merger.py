f40771fe661bf79a8e974999eb86fda8262a82ca
danielsanchezq <daniel.sanchez@iohk.io>
2021-11-08 10:48:52 +0100

    Python fmt

--- scripts/python/csv_merger.py
+++ scripts/python/csv_merger.py
@@ -8,7 +8,7 @@ from contextlib import contextmanager
 
 @contextmanager
 def open_files(files: Iterator[str]) -> Generator[Iterator[TextIO], None, None]:
-    files_objs = [open(file, encoding="utf-8", newline='') for file in files]
+    files_objs = [open(file, encoding="utf-8", newline="") for file in files]
     yield iter(files_objs)
     for file in files_objs:
         file.close()
@@ -23,9 +23,15 @@ def file_as_csv(file: TextIO, delimiter: chr) -> Tuple[List[str], Iterator[List[
     return next(reader), reader
 
 
-def merge_csv(pattern: str, base_path: Path, output_file: Path, input_delimiter: chr, output_delimiter: chr):
+def merge_csv(
+    pattern: str,
+    base_path: Path,
+    output_file: Path,
+    input_delimiter: chr,
+    output_delimiter: chr,
+):
     files = search_file_pattern(pattern, base_path)
-    with open(output_file, "w", encoding="utf-8", newline='') as out_file:
+    with open(output_file, "w", encoding="utf-8", newline="") as out_file:
         with open_files(files) as fs:
             writer = csv.writer(out_file, delimiter=output_delimiter)
             header, first_content = file_as_csv(next(fs), delimiter=input_delimiter)
@@ -38,11 +44,11 @@ def merge_csv(pattern: str, base_path: Path, output_file: Path, input_delimiter:
 
 
 def merge_csv_files(
-        output_file: Path = typer.Option(...),
-        pattern: str = typer.Option(...),
-        base_path: Path = typer.Option(default=Path("./")),
-        input_delimiter: str = typer.Option(default=','),
-        output_delimiter: str = typer.Option(default=',')
+    output_file: Path = typer.Option(...),
+    pattern: str = typer.Option(...),
+    base_path: Path = typer.Option(default=Path("./")),
+    input_delimiter: str = typer.Option(default=","),
+    output_delimiter: str = typer.Option(default=","),
 ):
     merge_csv(pattern, base_path, output_file, input_delimiter, output_delimiter)
 
