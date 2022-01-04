988d7526bd6cd4d6ef99f567c8c6c1a9f8d6096a
Giacomo Pasini <giacomo.pasini@iohk.io>
2022-01-04 11:22:05 +0100

    remove integration test

--- src/bin/cli/rewards/veterans.rs
+++ src/bin/cli/rewards/veterans.rs
@@ -54,34 +54,3 @@ impl VeteransRewards {
         Ok(())
     }
 }
-
-#[cfg(test)]
-mod tests {
-    use crate::cli::rewards::veterans::VeteransRewards;
-    use catalyst_toolbox::rewards::veterans::VeteranAdvisorReward;
-    use jcli_lib::utils::io;
-    use rust_decimal::prelude::FromStr;
-    use std::io::BufRead;
-
-    #[test]
-    fn test_output_csv() {
-        let resource_input = "./resources/testing/veteran_reviews_count.csv";
-        let tmp_file = assert_fs::NamedTempFile::new("outfile.csv").unwrap();
-
-        let export = VeteransRewards {
-            from: resource_input.into(),
-            to: tmp_file.path().into(),
-            total_rewards: 1000.into(),
-        };
-
-        export.exec().unwrap();
-        let reader = io::open_file_read(&Some(tmp_file.path())).unwrap();
-        let expected_reward = [50u32, 100, 200, 300, 350].map(VeteranAdvisorReward::from);
-        for (line, expected) in reader.lines().zip(expected_reward.iter()) {
-            let line = line.unwrap();
-            let res: Vec<&str> = line.split(',').collect();
-            let reward = VeteranAdvisorReward::from_str(res[1]).unwrap();
-            assert_eq!(&reward, expected);
-        }
-    }
-}
